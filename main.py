868424d2f24f4eff9b76810d0509b617c292f54f
unknown <andrbter@gmail.com>
2022-01-07 17:17:38 -0300

    Guardando a v1.0

---
+++ main.py
@@ -0,0 +1,333 @@
+# from sys import _clear_type_cache
+from models import *
+import time
+import pyautogui as pya
+import os
+
+def motoG5sPlus():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Moto G (5S) Plus\Armazenamento interno compartilhado\Download')
+    pya.press('enter')
+    print('Aparelho compativel')
+    print
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def motoG20():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\moto g(20)\Armazenamento interno compartilhado\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def motoG8Play():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\moto g(8) play\Armazenamento interno compartilhado\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyA10s():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Galaxy A10s\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyA11():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Galaxy A11\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def motoE6Plus():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\moto e(6) plus\Armazenamento interno compartilhado\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyA03s():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Galaxy A03s\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyS7():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Galaxy S7\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyS7v2():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Samsung Galaxy S7\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def motoG6Play():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\moto g(6) play\Armazenamento interno compartilhado\Download')
+    pya.press('enter')
+    time.sleep(1)
+
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+
+    print('Finalizando....')
+
+def galaxyS7Edge():
+    print('Loading.....')
+    pya.hotkey('win', 'e')
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta de aplicativos....')
+    pya.typewrite(r'C:\Users\Administrador\Documents\Apk')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','a')
+    pya.hotkey('ctrl','c')
+    time.sleep(1)
+    pya.hotkey('ctrl','l')
+    pya.hotkey('ctrl','a')
+    print('Abrindo pasta do celular...')
+    pya.typewrite(r'Este Computador\Galaxy S7 edge\Phone\Download')
+    pya.press('enter')
+    time.sleep(1)
+    pya.hotkey('ctrl','v')
+    time.sleep(2)
+    print('Finalizando....')
+    
+
+
+def main():
+    # Limpar tela do cmd
+    os.system('cls')
+
+    print('''Qual modelo de celular você quer transferir?
+    0 - Sair
+    01 - Moto G 5S Plus
+    02 - Moto G 20
+    03 - Moto G 8 Play 
+    04 - Galaxy A10s
+    05 - Galaxy A11
+    06 - Moto E 6 Plus
+    07 - Galaxy A03s
+    08 - Moto G 6 Play
+    09 - Galaxy S7
+    10 - Galaxy S7 (v2)
+    11 - Galaxy S7 Edge''')
+    opcaoDesejada = int(input('Digite o número correspondente ao modelo de celular que você quer transferir: '))
+    if opcaoDesejada == 1:
+        motoG5sPlus()        
+    elif opcaoDesejada == 2:
+        motoG20()
+    elif opcaoDesejada == 3:
+        motoG8Play()
+    elif opcaoDesejada == 4:
+        galaxyA10s()
+    elif opcaoDesejada == 5:
+        galaxyA11()
+    elif opcaoDesejada == 6:
+        motoE6Plus()
+    elif opcaoDesejada == 7:
+        galaxyA03s()
+    elif opcaoDesejada == 8:
+        motoG6Play()
+    elif opcaoDesejada == 9:
+        galaxyS7()
+    elif opcaoDesejada == 10:
+        galaxyS7v2()
+    elif opcaoDesejada == 11:
+        galaxyS7Edge()
+    elif opcaoDesejada == 0:
+        print('Saindo...')
+        time.sleep(2)
+        exit()
+    else:
+        print('Opção inválida!')
+    os.system('cls')
+    print('Loading.....')
+    time.sleep(5)
+    return main()
+    
+
+main()
+
+
+    
+
+
+
+
+
